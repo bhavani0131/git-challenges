77ab4c1276809c3a08f8e720f2e37d6485b66257
Durga Bhavani <durgabhavaniv77@gmail.com>
2026-05-22 12:23:14 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import boto3
+
+# Initialize the EC2 client
+ec2 = boto3.client('ec2', region_name='us-east-1')
+
+# Launch a single t2.micro instance
+response = ec2.run_instances(
+    ImageId='ami-0c7217cdde317cfec',  # Must match your region
+    InstanceType='t2.micro',
+    MinCount=1,
+    MaxCount=1
+)
+
+print(f"Launched instance: {response['Instances'][0]['InstanceId']}")
